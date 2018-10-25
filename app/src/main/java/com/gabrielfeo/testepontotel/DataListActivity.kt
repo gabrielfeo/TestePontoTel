865fd6ed74d327df83bf4415fd1fe6519c3235d9
Gabriel Feo <gabriel@gabrielfeo.com>
2018-10-25 13:28:08 -0300

    Initial commit
    Added auto-generated files from default Android Studio template.

---
+++ app/src/main/java/com/gabrielfeo/testepontotel/DataListActivity.kt
@@ -0,0 +1,12 @@
+package com.gabrielfeo.testepontotel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DataListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_list)
+    }
+}
